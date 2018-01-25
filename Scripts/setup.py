b87df8631aea1db0a6b95507e6f9e8907813a588
Avery Herkel <aherkel09@gmail.com>
2018-01-25 15:05:29 -0500

    Add files via upload

---
+++ Scripts/setup.py
@@ -0,0 +1,14 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {"packages": ["os"], "includes": ["tkinter"]}
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(  name = "ProExl",
+        version = "0.0.1",
+        description = "Write HVAC Database Items to Excel",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("ProExl.py", base=base)])
