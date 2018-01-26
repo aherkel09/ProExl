8a3dad25e19825bff2e17a5dbba91db99b7aa487
Avery Herkel <aherkel09@gmail.com>
2018-01-26 16:29:41 -0500

    Update setup.py

--- Scripts/setup.py
+++ Scripts/setup.py
@@ -1,14 +1,16 @@
 import sys
 from cx_Freeze import setup, Executable
 
-build_exe_options = {"packages": ["os"], "includes": ["tkinter"]}
+include_files = ['formatExl.py', 'makeDuct.py']
+build_exe_options = {"packages": ["os"], "includes": ["tkinter"],
+                     "include_files": include_files}
 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
 setup(  name = "ProExl",
-        version = "0.0.1",
+        version = "0.0.2",
         description = "Write HVAC Database Items to Excel",
         options = {"build_exe": build_exe_options},
         executables = [Executable("ProExl.py", base=base)])
