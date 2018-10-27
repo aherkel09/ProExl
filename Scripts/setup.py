3eeda1d7f0847cbe1d3b94b7afd161b1a6372743
Avery Herkel <aherkel09@gmail.com>
2018-10-26 22:11:32 -0400

    reformat

--- Scripts/setup.py
+++ Scripts/setup.py
@@ -2,14 +2,21 @@ import sys
 from cx_Freeze import setup, Executable
 
 include_files = ['README.txt', 'Templates', 'tcl86t.dll', 'tk86t.dll', 'makeDuct.py', 'formatExl.py']
-build_exe_options = {"packages": ["os"], "includes": ["tkinter"],
-                     "include_files": include_files}
+
+build_exe_options = {
+    "packages": ["os"], 
+    "includes": ["tkinter"],
+    "include_files": include_files
+}
 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
-setup(  name = "ProExl",
-        version = "0.0.2",
-        description = "Write HVAC Database Items to Excel",
-        options = {"build_exe": build_exe_options},
-        executables = [Executable("ProExl.py", base=base)])
+
+setup(
+    name = "ProExl",
+    version = "0.0.2",
+    description = "Write HVAC Database Items to Excel",
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("ProExl.py", base=base)]
+)
